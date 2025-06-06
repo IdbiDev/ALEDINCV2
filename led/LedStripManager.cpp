c629c3614cf8aaa3bf9b2713bf3e10de2be5054f
Idbi <idbi.developer@gmail.com>
2025-06-06 13:02:25 +0200

    Add files via upload

---
+++ led/LedStripManager.cpp
@@ -0,0 +1,24 @@
+#include "LedStripManager.hpp"
+
+uint8_t LedStripManager::get_next_id(){
+    for (uint8_t i = 0; i < MAX_LED_AMOUNT; i++)
+    {
+        if(id_list[i] == false){
+            id_list[i] = true;
+            return i
+        }
+    }
+    return -1;
+}
+
+
+void LedStripManager::free_id(uint8_t id){
+    id_list[id] = false;
+}
+
+LedStripManager::LedStripManager() {
+    for (uint8_t i = 0; i < MAX_LED_AMOUNT; i++)
+    {
+            id_list[i] = false;
+    }
+}
---
+++ led/LedStripManager.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <cstdint>
+#include "Utils.hpp"
+#include <string>
+#include "LedStrip.hpp"
+#define MAX_LED_AMOUNT 8
+
+class LedStripManager{
+public:
+    LedStripManager();
+    uint8_t get_next_id();
+    void free_id(uint8_t id);
+    
+    LedStrip led_strips[MAX_LED_AMOUNT];
+private:
+    bool id_list[MAX_LED_AMOUNT];
+
+    
+}
